4eadc2b9d8997ff0734d3a4e1f2285e8ef3e95c0
Maria <blueiceshards@gmail.com>
2020-05-12 22:33:28 +0800

    ssh notes

---
+++ SSH/ssh-notes.js
@@ -0,0 +1,14 @@
+/* 
+LESSON 1: INTRODUCTION
+
+SSH, or secure shell, is a protocol. (other protocols: HTTP, FTP, HTTPS.) These are all ways to connect to computers and have a shared agreement of how to communicate. SSH is a way for machines to communicate with one another, between two computers over the internet. SSH allows users to share files as well as control and modify remote computers over the internet. It was created as a secure way of communication which encrypts all data so bad actors can't do bad things. 
+
+What's the difference between HTTPS? Just like a web browser uses the HTTPS protocol to talk to servers and display websites, a shell needs a certain protocol to allow for data exchange or communication between two devices and not just the browser and the server. This is why SSH is called secure SHELL protocol. It's a protocol to use over shells. A shell, unlike the browser, allows you to talk to the OS. With SSH, I can use my terminal to talk to another computer. 
+
+The significant advantage of SSH over its predecessors is the use of encrpytion to ensure the secure transfer of information between the host and the client. Host: remote server you're trying to access. Client: computer you are using to access the host. Bad guys can't read our messages because they are secure. 
+
+LESSON 2: SSH COMMAND
+
+
+
+*/

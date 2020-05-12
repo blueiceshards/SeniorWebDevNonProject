30f0b7eed4b863346279fb378aaa5b34d850d869
Maria <blueiceshards@gmail.com>
2020-05-12 22:18:42 +0800

    introduction lesson

---
+++ Introduction/notes.js
@@ -0,0 +1,15 @@
+/* Course Overview 
+- SSH
+- Performance
+- React and Redux
+- Testing
+- TypeScript
+- Single Page Applications vs Server Side (Rendering)
+- Security
+- Docker
+- Redis (Database)
+- Sessions + JWT
+- AWS
+- Continuous Integration and Continuous Delivery
+- Code Analysis and Best Practices
+*/

01df1bd650f24d7f77f550666523010a38447445
Maria <blueiceshards@gmail.com>
2020-05-13 14:50:53 +0800

    performance 1

---
+++ Performance1/performanceintro-notes.js
@@ -0,0 +1,15 @@
+/* Performance Part 1
+
+LESSON 1: INTRODUCTION TO PERFORMANCE PART 1 
+
+For big companies, page load speeds affect revenues by a lot. 
+
+LESSON 2: KEYS TO PERFORMANCE
+
+there are 3 ways to improve the speed of your website. (1) improve what happens on the client side i.e. front end, because the front end device needs time to render the page on the screen. (2) improve the transfer of our files over the wire. network latency, which is the time it takes for request to travel from the client to the server, and back. This becomes even more essential when we consider that an average website needs more than a hundred requests to load completely. (3) improve the processing done on the backend, i.e. back end processing. the web server needs time to load data, maybe from the database, and maybe even assemble a website before it sends it over. for our website to be as fast as possible, we can improve something in all 3 of these sections. there are infinite ways to optimize our website for our app. but one thing to keep in mind is that we want to think like a senior developer -> focus on a problem and find a solution to solve the problem in the most efficient and valuable way. if you are optimizing the site but don't know what is slwoing it down, and don't have comparison numbers or benchmarks to see if your optimizations have actually improved your speed, then you're just wastingg your time. 
+
+In this course, we will focus on the big ticket items, the main performance improvements that will improve 90% of your website speeds, and how to implement them in your code. For Part 1, we will focus on the network transfer, and the critical render path. Part 2, we will write optimized code and progressive web apps. For Part 3, we will cover backend things to improve performance and scale. 
+
+
+
+*/

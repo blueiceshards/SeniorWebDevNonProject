84ce67bbdd82e7016d8c6a95e27d28178c5e77fe
Maria <blueiceshards@gmail.com>
2020-05-14 21:50:39 +0800

    react notes

---
+++ ReactRedux/React-notes.js
@@ -0,0 +1,18 @@
+/* 
+Angular, React and ViewJS are the top 3 frameworks and libraries to build your front-end applications today, all of them with the goal of managing large applications on the front end that have a lot of javascript. they were built to solve the problem, for companies to be able to hire develoeprs and make sure that these developers can handle many lines of code in a nice, clean, way. there are plenty of articles out there comparing these tools. there is no shortage of these articles. these libraries and frameworks are just tools. each one of them is perfect for its own specific use case. you will always find people who will only use one library and hate the others, but this is never a good way of doing things, especially when it comes to these front end tools. each one of them has their pros and cons, and a senior developer only ares which one of them is good for the job at hand. avoid following a library as a solution to all the problems in the world - this simply will not exist. there is never a tool that solve everything. tools change, and library evolves. if you use View.JS now and in the future when there is an even better tool and things change, you need to be able to adapt. Think of these as tools, each one with its pros and cons - pick tools accordingly. 
+
+Currently, his current thinking process to pick frameworks: if he's working for a large bank with massive codebase, he will pick angular - framework, an entire kitchen, with its own way of doing things. opinionated - specific way of doing things. a lot of developers like a bank with a massive project -> you can ensure taht everyone sticks to the rules. everybody is working in the same kitchen with the same tools. 
+
+React - for a company with a strong developer team and also needs to be flexible and evolve by adding different libraries and tools to the ecosystem. can trust developers to build on top of react with all these different tools something flexible that works for his company.
+
+view - if we want a simple way of writing code that is friendly, easy to pick up if you hire a lot of junior developers. 
+
+these are just his opinions, but start thinking this way, in terms of pros and cons and not absolutes (this sucks..)
+
+For now, react is a clear winner. it stands above all the others for now (indeed.com: job trends % of matchnig job postings) Even though vue is really popular with the develoer community, perhaps wait a little until it picks up in job demands before joining the hype. 
+ 
+react - in demand right now, but also teachs really good JS principles that last a lifetime. with angular, this is hard to do, and vue is still young and growing. 
+
+angular - the entire kitchen will all the tools for everyone to cook in the same ktichen. react - the oven. you need other tools on top of it to cool something, but react offers us more flexibility and creative freedom. vue - the microwave. super simple, easy to pick up.  
+
+*/
